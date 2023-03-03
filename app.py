3a10abcbc6d55c9d85609690dcf0595298ee97b8
nidhibpatel <npatel29@emich.edu>
2023-03-03 18:34:35 -0500

    added results.html

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('hello.html')
+    
+@app.route('/results')
+def results():
+    return render_template('results.html')
 
 if __name__ == "__main__":
     app.run()
