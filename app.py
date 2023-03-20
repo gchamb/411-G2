7e62d2f86ad2810e8fb886498a1f4f7d4d75466d
Sam S <sstone20@emich.edu>
2023-03-20 18:52:41 -0400

    Basic server-model connection

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template, request
+import json
+import pickle
+import datetime as dt
 
 
 app = Flask(__name__)
@@ -10,11 +13,39 @@ def index():
     
 @app.route('/results', methods=['POST'])
 def results():
-    Cname = (request.form['Cname']) 
-    Cname_result = Cname
+    ticker = (request.form['Cname']) 
+    dateStr = request.form['start']
 
-    Date = (request.form['start'])
-    return render_template('results.html', result= Cname_result + " " + Date) 
+    print(ticker)
+    print(dateStr)
+
+    # validate ticker
+    with open('stocks.json','r') as file:
+        l = json.loads(file.read())
+
+    found = False
+    for t in l:
+        print(t)
+        print(t == ticker)
+        print(type(t))
+        print(type(ticker))
+        if t == ticker:
+            found = True
+            break
+
+    if not found:
+        return "Bad ticker" #
+
+    # load model
+    model = pickle.load(open(f'./models/{ticker}-model.pkl','rb'))
+    
+    # predict
+    date = dt.datetime.fromisoformat(dateStr).toordinal()
+    print(date)
+    prediction = model.predict([[date]])
+    print(prediction[0][0])
+
+    return render_template('results.html', ticker = ticker, date = dateStr, price = prediction[0][0]) 
 
 
     
